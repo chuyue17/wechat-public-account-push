14b84bd42bac849550076fe3d93a547d100ffb65
zrf210 <100744630+zrf210@users.noreply.github.com>
2022-09-07 14:10:32 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,20 +31,28 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "嘿嘿~~~",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "o0PP46aABbNeqszmtm4PdNCHhkx8",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "KV9T6_Q7Meksri6eqPtJZAQlzkOEf8TGyeP9A3jw-Xo",
       // 所在省份
-      province: "广东",
+      province: "辽宁",
       // 所在城市
-      city: "肇庆",
+      city: "朝阳",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
     {
-      name: "老婆1", 
+      name: "嘿嘿~~~", 
+      id: "o0PP46XZleR9ypfCF_UgL7RsKHxk",
+      useTemplateId: "KV9T6_Q7Meksri6eqPtJZAQlzkOEf8TGyeP9A3jw-Xo",
+      province: "河北",
+      city: "固安",
+      openUrl: "https://wangxinleo.cn"
+    }, 
+    {
+      name: "嘿嘿2",
       id: "",
       useTemplateId: "",
       province: "",
@@ -52,15 +60,7 @@ export const config = {
       openUrl: "https://wangxinleo.cn"
     }, 
     {
-      name: "老婆2",
-      id: "",
-      useTemplateId: "",
-      province: "",
-      city: "",
-      openUrl: "https://wangxinleo.cn"
-    }, 
-    {
-      name: "老婆3", 
+      name: "嘿嘿3", 
       id: "",
       useTemplateId: "",
       province: "",
@@ -74,7 +74,7 @@ export const config = {
    */
 
   // 回调消息模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: "zjV1glIuCXo4Csr23aXWEio1iP74sZJ7IQEES95lDsg6c4yOU",
+  CALLBACK_TEMPLATE_ID: "KV9T6_Q7Meksri6eqPtJZAQlzkOEf8TGyeP9A3jw-Xo",
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -82,7 +82,7 @@ export const config = {
       // 一般都填自己
       name: "自己",
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohx12OO6VskYv03Kuf43ywIZVRM4puXhk",
+      id: "o0PP46XZleR9ypfCF_UgL7RsKHxk",
     }, 
   ],
     
@@ -106,11 +106,11 @@ export const config = {
    * 节日时，name填写相应展示的节日，你可以填“被搭讪纪念日”
    */
   FESTIVALS: [
-    {"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
-    {"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
-    {"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
-    {"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
-  ],
+    //{"type": "生日", "name": "老婆", "year": "1996", "date": "09-02"},
+    //{"type": "节日", "name": "结婚纪念日", "year": "2020", "date": "09-03"},
+    //{"type": "生日", "name": "李四", "year": "1996", "date": "09-31"},
+    //{"type": "节日", "name": "被搭讪纪念日", "year": "2021", "date": "09-01"},
+  //],
 
   /**
    * 限制重要节日的展示条目, 需要填写数字; 
@@ -127,11 +127,11 @@ export const config = {
     * */
   CUSTOMIZED_DATE_LIST: [
     // 在一起的日子
-    {"keyword": "love_day", date: "2015-05-01"},
+    //{"keyword": "love_day", date: "2015-05-01"},
     // 结婚纪念日
-    {"keyword": "marry_day", date: "2020-01-04"},
+    //{"keyword": "marry_day", date: "2020-01-04"},
     // 退伍日, 不用可以删掉
-    {"keyword": "ex_day", date: "2022-08-31"},
+    //{"keyword": "ex_day", date: "2022-08-31"},
     // sakana日
     // {"keyword": "sakana_day", date: "2022-01-06"},
     // ... 
@@ -148,10 +148,10 @@ export const config = {
     {"keyword": "encourage_oneself", contents: "你主要的问题在于读书太少而想得太多"},
     // 这样配置的话，就会每次随机选一句话发送
     {"keyword": "lover_prattle", contents: [
-      "因为太喜欢你，所以看谁都像是情敌。",
-      "申请成为你爱里的永久居民。",
-      "你很傻，你很笨，可我还是很羡慕你，因为你有我",
-      "遇见你，就好像捡到了100斤的运气",
+      "太6了",
+      "666。",
+      "相见亦无事 别后常忆君",
+      "秋成落叶叶成思，叶落相思人不知",
     ]},
   ],
 
